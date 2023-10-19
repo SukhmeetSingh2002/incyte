994f18429d0623ff0fe26a71a19e59938b5c1981
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2023-10-19 10:30:51 +0530

    minor changes

--- setup.py
+++ setup.py
@@ -17,12 +17,9 @@ setup(
         "rich",
     ],
     python_requires=">=3.6",
-    author="Siddharth Dushantha",
-    author_email="youremail@example.com",  # Replace with your email
     description="A stress testing tool for comparing the output of two programs",
     long_description=long_description,
-    long_description_content_type="text/markdown",  # Specify the content type
+    long_description_content_type="text/markdown",  
     license="MIT",
     keywords="compare, output, diff, stress testing, testing, software testing, stress tests, stress test tool",
-    url="https://example.com/incyte",   # Replace with the project home page URL
 )
